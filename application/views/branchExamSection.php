3a15ae792423766eca5f022d43d31b045ba7557f
imamityadav <amityadav2712@gmail.com>
2019-06-15 17:17:39 +0530

    exam section processing

--- application/views/branchExamSection.php
+++ application/views/branchExamSection.php
@@ -1,1 +1,26 @@
-hiiii
+<div class="row">
+	<div class="col-md-12">
+		 
+		<h3><?php echo $subtitle;?></h3>
+		
+			 <div class="panel panel-calendar  exam_panel_body">
+                <div class="panel-heading bg_info border-light">
+                    <h5 class="panel-title text-center">Exam Section</h5>
+                </div>
+               <div class="panel-body">
+               <div class="text-black text-large exam_panel">
+               		<div class="row">
+               			<div class="col-md-12">
+               				<div class="row">
+               					<div class="col-md-4"></div>
+               					<div class="col-md-4"></div>
+               					<div class="col-md-4"></div>
+               				</div>
+               			</div>
+               		</div>
+               </div>
+           </div>
+       </div>
+
+	</div>
+</div>
